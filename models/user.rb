04cf0a78e2c92e0e8df7d3272c122151a5072098
Leandro Guida <lcguida@gmail.com>
2014-06-08 20:32:17 -0300

    Melhorias no código

--- models/user.rb
+++ models/user.rb
@@ -4,4 +4,16 @@ class User
 	property :login, String
 	property :firstname, String
 	property :lastname, String
+	property :hashed_password, String
+	property :salt, String
+	property :admin, Boolean
+
+	#Autentica a senha do usuário
+	def authenticate(password)
+		Digest::SHA1.hexdigest("#{salt}#{Digest::SHA1.hexdigest(password)}") == hashed_password
+	end
+
+	def name
+		"#{firstname} #{lastname}"
+	end
 end
